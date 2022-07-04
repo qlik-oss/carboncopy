b67b0158b1f2c1707543ee6b834463df7bf1f754
Vittorio Cellucci <vel@qlik.com>
2022-07-04 07:53:46 -0400

    fix: add import statement

--- bin/setupExample.js
+++ bin/setupExample.js
@@ -4,6 +4,14 @@ var fs = require('fs');
 const args = process.argv.slice(2);
 console.log(args);
 const myShellScript = exec(`npx react-native init ${args[0]}  --template https://github.com/qlik-oss/carboncopy`);
+
+function insertImport() {
+  const filePath = `${args[0]}/src/components/Visualization.tsx`;
+  const data = fs.readFileSync(filePath).toString().split("\n");
+  data.splice(7, 0, `import supernova from '${args[2]}';`);
+  const  text = data.join("\n");
+  fs.writeFileSync(filePath, text, {encoding: 'utf-8'});
+}
 myShellScript.stdout.on('data', (data)=>{
     console.log(data);
     // do whatever you want here with data
@@ -15,4 +23,5 @@ myShellScript.stderr.on('data', (data)=>{
 myShellScript.on('exit', () => {
   const snType = args[1];
   fs.writeFileSync(`${args[0]}/config.json`, JSON.stringify({snType}), {encoding: 'utf-8'});
+  insertImport();
 });
